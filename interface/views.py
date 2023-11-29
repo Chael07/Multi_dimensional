fd93571f01d29ed2241a38d368abe0692ad26eb2
Chael07 <202080469@psu.palawan.edu.ph>
2023-11-29 17:09:18 +0800

    initial commit

---
+++ interface/views.py
@@ -0,0 +1,20 @@
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+# from .models import Contact
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def home_screen_view(request):
+	print(request.headers)
+	return render(request, "index.html", {})
+
+def privacy_screen_view(request):
+	print(request.headers)
+	return render(request, "privacy.html", {})
+
+def condition_screen_view(request):
+	print(request.headers)
+	return render(request, "terms.html", {})
